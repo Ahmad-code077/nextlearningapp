c516673454bd8686d155a16630b6c1d220e99af3
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-09-13 10:39:23 +0500

    added some changes

---
+++ app/components/TaskList.jsx
@@ -0,0 +1,40 @@
+import prisma from '@/utils/db';
+import Link from 'next/link';
+import DeleteForm from './DeleteForm';
+
+const TaskList = async () => {
+  const Tasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  if (Tasks.length <= 0) {
+    return <h1 className='text-xl'> NO task Found </h1>;
+  }
+  return (
+    <ul className='mt-8'>
+      {Tasks.map((task) => (
+        <li
+          key={task.id}
+          className='flex justify-between items-center px-6 py-4 mb-4 border border-base-300 rounded-lg shadow-lg'
+        >
+          <h2
+            className={`capitalize text-lg  ${
+              task.completed ? 'line-through' : null
+            } `}
+          >
+            {task.content}
+          </h2>
+          <div className='flex items-center gap-4'>
+            <Link href={`/tasks/${task.id}`} className='btn  btn-accent btn-xs'>
+              Edit
+            </Link>
+            <DeleteForm id={task.id} />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default TaskList;
--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,4 +1,12 @@
+import TaskForm from '../components/TaskForm';
+import TaskList from '../components/TaskList';
+
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <div className='max-w-lg'>
+      <TaskForm />
+      <TaskList />
+    </div>
+  );
 };
 export default Tasks;
