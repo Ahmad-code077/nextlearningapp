3aed558991c125b02e3c9a874b741a85a154a3e9
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-09-16 12:24:36 +0500

    applied middlle ware in next js

--- app/components/DeleteForm.jsx
+++ app/components/DeleteForm.jsx
@@ -1,9 +1,22 @@
+'use client';
 import { deleteTask } from '../../utils/actions';
+
+import { useFormStatus } from 'react-dom';
+
+const DeleteButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className='btn btn-error btn-xs'>
+      {pending ? 'pending.....' : 'Delete'}
+    </button>
+  );
+};
+
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-error btn-xs'>delete</button>
+      <DeleteButton />{' '}
     </form>
   );
 };
